094a034bf7c72da7649ece80b496c423d0fd5b64
jezy <jezy@tyto.pl>
2017-12-08 13:15:15 +0100

    Inne ankiety

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+app_name = 'polls'
 
 urlpatterns = [
     # ex: /polls/
@@ -8,7 +9,7 @@ urlpatterns = [
     # # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
     # # ex: /polls/5/results/
-    # path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/results/', views.results, name='results'),
     # # ex: /polls/5/vote/
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
